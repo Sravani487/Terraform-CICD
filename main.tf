b77570e867059e814e6f18a932f11ae1fef10a6a
Sravani487 <sravani.v487@gmail.com>
2025-11-22 12:38:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "dev" {
+    ami = "ami-0fa3fe0fa7920f68e"
+    instance_type = "t3.micro"
+    tags = {
+      Name = "Test Ec2"
+    }
+}
